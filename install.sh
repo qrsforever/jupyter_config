b45cbe020d9fc8fbb8d585f5118faf40d58bcccb
Li Dong <705723886@qq.com>
2021-10-25 15:22:19 +0800

    save

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-CUR_DIR=`dirname ${BASH_SOURCE[0]}`
+CUR_DIR=`pwd $(cd $(dirname ${BASH_SOURCE[0]}))`
 
 NB_CONFIG_DIR=$(jupyter --config-dir)
 NB_DATA_DIR=$(jupyter --data-dir)
 
-ln -s $CUR_DIR/jupyter $NB_CONFIG_DIR/
+ln -s $CUR_DIR/jupyter $NB_CONFIG_DIR
 ln -s $CUR_DIR/local/share/jupyter $NB_DATA_DIR
 
 # cp -aprf $CUR_DIR/jupyter/* $NB_CONFIG_DIR/
