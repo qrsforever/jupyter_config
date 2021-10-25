0ded9b19fa855d232ac73ad422a2b6c992933087
Li Dong <705723886@qq.com>
2021-10-25 15:16:17 +0800

    asve

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ CUR_DIR=`dirname ${BASH_SOURCE[0]}`
 NB_CONFIG_DIR=$(jupyter --config-dir)
 NB_DATA_DIR=$(jupyter --data-dir)
 
-cp -aprf $CUR_DIR/jupyter/* $NB_CONFIG_DIR/
-cp -aprf $CUR_DIR/local/share/jupyter/* $NB_DATA_DIR/
+ln -s $CUR_DIR/jupyter $NB_CONFIG_DIR/
+ln -s $CUR_DIR/local/share/jupyter $NB_DATA_DIR
+
+# cp -aprf $CUR_DIR/jupyter/* $NB_CONFIG_DIR/
+# cp -aprf $CUR_DIR/local/share/jupyter/* $NB_DATA_DIR/
